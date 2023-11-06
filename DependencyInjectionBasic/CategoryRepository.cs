625488e00e3b6ed3c9372666379afc195ccf9244
PranaySpp98 <pranay.pp98@gmail.com>
2023-11-07 00:38:21 +0530

    Add project files.

---
+++ DependencyInjectionBasic/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionBasic
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            Category category = new Category() { CategoryId = 1, CategoryName = "Food" };
+            categories.Add(category);
+            category = new Category() { CategoryId = 2, CategoryName = "SW" };
+            categories.Add(category);
+
+            return categories;
+        }
+    }
+}
---
+++ DependencyInjectionBasic/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionBasic.Controllers
+{
+    [Route("api/Category")]
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository category { get; set; }
+        public CategoryController(ICategoryRepository category)
+        {
+            this.category = category;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Category> categories = category.GetCategories();
+            return Ok(categories);
+        }
+    }
+}
